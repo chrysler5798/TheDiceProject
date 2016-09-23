e701a5a1f39a05f5e19c4e6924a9481f6f128823
chrysler5798 <chrysler5798@hotmail.fr>
2016-09-23 17:05:26 +0200

    Added SetTitle MainActivity Added Options for the Menu in MainActivity Added main_menu.xml

--- app/src/main/java/com/khrys/thediceproject/MainActivity.java
+++ app/src/main/java/com/khrys/thediceproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.khrys.thediceproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("The Dice Project");
 
         buttonDe6 = (Button) findViewById(R.id.buttonDe6);
         buttonDe20 = (Button) findViewById(R.id.buttonDe20);
@@ -39,5 +43,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+            /* DO EDIT */
+                return true;
+            case R.id.action_add:
+            /* DO ADD */
+                return true;
+            case R.id.action_delete:
+            /* DO DELETE */
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
